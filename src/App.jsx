fefa522aed8560c69140506d576fd0bff2ac0112
Sathvik1109 <saathvikdugyala09@gmail.com>
2026-02-22 17:40:42 -0600

    Cleaned up Data Fetching

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
             `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal },
           );
 
           if (!res.ok)
@@ -33,8 +36,11 @@ export default function App() {
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movie Not Found");
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -47,6 +53,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return () => {
+        controller.abort();
+      };
     },
     [query],
   );
